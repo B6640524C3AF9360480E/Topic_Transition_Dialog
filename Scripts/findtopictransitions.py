35ce11b40aa8f158828d08ff38fae230255393c9
B6640524C3AF9360480E <58304557+B6640524C3AF9360480E@users.noreply.github.com>
2020-11-23 17:32:15 +0530

    Add files via upload

---
+++ Scripts/findtopictransitions.py
@@ -0,0 +1,18 @@
+'''
+Input to the file is utterances grouped by speaker, Output is marking chunks where topic transition
+takes place
+'''
+
+'''
+Using the topic transitions, the code then outputs topical segments
+'''
+
+'''
+The results can then be found in the output folder
+'''
+
+
+'''
+arguements 
+#input file name in csv format containing columns utterance_chunks
+'''
